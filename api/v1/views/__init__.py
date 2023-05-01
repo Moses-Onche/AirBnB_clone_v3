e26cd5d85fb61ab716cee4eef574e6b6ceee20e3
Moses-Onche <onchemoses38@gmail.com>
2023-05-01 13:35:19 +0100

    Create Blueprint for flask API and index view

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Defines blueprint representation for API.'''
+"""Defines blueprint representation for API."""
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 """Defines the index page."""
 from flask import jsonify
-from models import storage
 from api.v1.views import app_views
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.user import User
 
 
 @app_views.route('/status')
 def get_status():
-    '''Gets the status of the API.
-    '''
+    """Get status of API."""
     return jsonify(status='OK')
