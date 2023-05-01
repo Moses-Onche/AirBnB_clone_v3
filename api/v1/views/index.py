07a76e2fc680c7c07fa765a8ff12e1c2473886f9
Moses-Onche <onchemoses38@gmail.com>
2023-05-01 13:44:57 +0100

    create endpoint to retrieve number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,18 @@ from api.v1.views import app_views
 def get_status():
     """Get status of API."""
     return jsonify(status='OK')
+
+@app.route('/stats')
+def get_num_objects():
+    """Get number of objects by type."""
+    obj = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, item in obj.items():
+        obj[key] = storage.count(item)
+    return jsonify(obj) 
