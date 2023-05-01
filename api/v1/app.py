353e3c2f2001db3141cc0678dd2af3119d1f5288
Moses-Onche <onchemoses38@gmail.com>
2023-05-01 13:25:23 +0100

    Create Blueprint for flask API and index view

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,29 +3,20 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import OS
+import os
 
 app = Flask(__name__)
 """Instance of the Flask web app."""
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', '5000'))
 
 
 @app.teardown_appcontext
 def teardown_flask(exception):
     '''The Flask app/request context end event listener.'''
-    # print(exception)
     storage.close()
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    app.run(host=host, port=port, threaded=True)
